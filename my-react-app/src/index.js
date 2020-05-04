a5cb7c5fd21faa1d2f9155dacf7777fff1525cd2
neel <neel@trisysit.com>
2020-05-05 00:45:45 +0530

    check cmt

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () =>{
-  return <h1>Hi React</h1>
+  return <h1>Hi React !!</h1>
 }
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
